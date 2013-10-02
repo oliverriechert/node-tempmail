e2fbfc80bcf0cfcd8c7739a00ab784a738786c0d
Trevor Senior <trevor@urban4m.com>
2013-10-02 18:10:24 -0400

    using

--- index.js
+++ index.js
@@ -4,50 +4,31 @@ var Provider = require('./src/Provider');
 
 module.exports = Provider;
 
-// CLI
-var exit = function(error, out) {
-	if (error) {
-		console.error(error.stack || error); process.exit(1);
-	} else if (out) {
-		if (typeof out === 'object') {
-			try { var json = JSON.stringify(out); } catch(e) {}
-		}
-		process.stdout.write(json || out);
-	}
-	process.exit(0);
-};
+require('main')
+.usage(['Usage: ./tempmail <provider> [emailAddress]', '',
+	'- By specifying only a provider, it will return a new email address.',
+	'- Specifying an email will return the inbox (JSON).'
+].join('\n'))
+.run(function(argv, exit, help) {
+	var providerName = argv._[0];
+	var emailAddress = argv._[1];
+	if (!providerName) { exit(1, help); }
 
-var main = function() {
-	var provider = process.argv[2];
-	var emailAddress = process.argv[3];
-
-    if (!provider) {
-    	exit([
-    	'Usage: ./tempmail <provider> [emailAddress]', '',
-    	'- By specifying only a provider, it will return a new email address.',
-    	'- Specifying an email will return the inbox (JSON).'
-    	].join('\n'));
-	}
-
-	var provider = new Provider(provider);
+	var provider = new Provider(providerName);
 
 	if (!emailAddress) {
 		// create a new email
 		provider.newTempEmail(provider).then(function(tempEmail) {
 			exit(null, tempEmail.getAddress());
 		}).otherwise(function(error) {
-			exit(error);
-		})
+			exit(1, error);
+		});
 	} else {
 		// fetch the inbox
 		provider.readEmailAddress(emailAddress).then(function(inbox) {
 			exit(null, inbox);
 		}).otherwise(function(error) {
-			exit(error);
-		})
+			exit(1, error);
+		});
 	}
-}
-
-if (require.main === module) {
-	try { main(); } catch(e) { exit(e); }
-}
+});
